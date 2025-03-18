22a7819d93061f323c9faa7d499b5a9a447d3a62
heekari <hantecude123@gmail.com>
2025-03-18 19:37:07 +0900

    1

--- FileName.cpp
+++ FileName.cpp
@@ -1,54 +1,72 @@
 #include<opencv2/opencv.hpp>
 
+void prob1(IplImage* src);
+void prob2(IplImage* src);
+void prob3(IplImage* src);
+
 int main() {
 	IplImage* src = cvLoadImage("c:\\temp\\sejong_small.jpg");
+	prob1(src);
+	prob2(src);
+	prob3(src);
+	cvShowImage("src", src);
+	cvWaitKey();
 
-	IplImage* rev = cvCreateImage(cvGetSize(src), 8, 3);
-	IplImage* half = cvCreateImage(cvGetSize(src), 8, 3);
-	IplImage* sqa = cvCreateImage(cvGetSize(src), 8, 3);
-	IplImage* grc = cvCreateImage(cvGetSize(src), 8, 3);
-	IplImage* blc = cvCreateImage(cvGetSize(src), 8, 3);
-	IplImage* bch = cvCreateImage(cvGetSize(src), 8, 3);
+	return 0;
+}
 
-	// 1번 이미지
+void prob1(IplImage* src) {
+	IplImage* tar = cvCreateImage(cvGetSize(src), 8, 3);
 	for (int y = 0; y < src->height; y++) {
 		for (int x = 0; x < src->width; x++) {
 			CvScalar ref = cvGet2D(src, y, x);
-			cvSet2D(rev, rev->height - y - 1, rev->width - x - 1, ref);
+			cvSet2D(tar, tar->height - y - 1, tar->width - x - 1, ref);
 		}
 	}
+	//cvShowImage("prob-1", tar);
+}
 
-	// 2번 이미지
+void prob2(IplImage* src) {
+	IplImage* tar = cvCreateImage(cvGetSize(src), 8, 3);
+
 	for (int y = 0; y < src->height; y++) {
 		for (int x = 0; x < src->width; x++) {
 			CvScalar ref = cvGet2D(src, y, x);
 			if (x < src->width / 2) {
-				cvSet2D(half, y, src->width / 2 + x, ref);
+				cvSet2D(tar, y, src->width / 2 + x, ref);
 			}
 			else {
-				cvSet2D(half, y, x - src->width / 2, ref);
+				cvSet2D(tar, y, x - src->width / 2, ref);
 			}
 		}
 	}
+	//cvShowImage("prob-1", tar);
+}
 
+void prob3(IplImage* src) {
+	IplImage* tar = cvCreateImage(cvGetSize(src), 8, 3);
 	for (int y = 0; y < src->height; y++) {
 		for (int x = 0; x < src->width; x++) {
 			CvScalar ref = cvGet2D(src, y, x);
-
 			// greyscale
 			CvScalar grey;
 			int bri = (ref.val[0] + ref.val[1] + ref.val[2]) / 3;
 			for (int k = 0; k < 3; k++)
 				grey.val[k] = bri;
-
-			
-
+			cvSet2D(tar, y, x, grey);
 		}
 	}
 
-	cvShowImage("src", src);
-	cvShowImage("reverse", sqa);
-	cvWaitKey();
+	float lean = (float)(src->width / 2) / (src->height / 2);
+	int cp = 0;
+	bool isCrossed = false;
+	for (int y = 0; y < src->height; y++) {
+		for (int x = 0; x < src->width; x++) {
+			
+			if (isCrossed) continue;
+			
+		}
+	}
 
-	return 0;
+	cvShowImage("prob-3", tar);
 }
